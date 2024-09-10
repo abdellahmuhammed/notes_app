2624b6aaadc20ba7545edbad6ddc02ae0f477fbd
Your Name <youremail@example.com>
2024-09-10 15:19:29 +0300

    Provide and Consume AddNoteCubit

--- lib/Views/Widgets/AddNoteShowModalBottomSheet.dart
+++ lib/Views/Widgets/AddNoteShowModalBottomSheet.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: file_names
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/Views/Widgets/AddNoteForm.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 
 class AddNoteShowModalBottomSheet extends StatelessWidget {
@@ -9,10 +10,20 @@ class AddNoteShowModalBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const  Padding(
-      padding: EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
       child: SingleChildScrollView(
-        child:AddNoteForm(),
+        child: BlocProvider(
+          create: (context) => AddNoteCubit(),
+          child: BlocConsumer<AddNoteCubit, AddNoteState>(
+            listener: (context, state) {
+              // TODO: implement listener
+            },
+            builder: (context, state) {
+              return const AddNoteForm();
+            },
+          ),
+        ),
       ),
     );
   }
